f2daf8c56762a3ad452546ad1cc495253235d334
Erhan Özgüç <eozguc@hotmail.com>
2022-05-31 16:25:35 +0300

    Add files via upload

---
+++ FirstReverse/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstReverse;
+
+public class Program
+{
+    public static string FirstReverse(string str) {
+
+        // code goes here  
+        //return str;
+        
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+
+    }
+
+    static void Main() {  
+        // keep this function call here
+        Console.WriteLine(FirstReverse(Console.ReadLine()));
+    } 
+}
