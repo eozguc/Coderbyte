a7808d7f00bd85eb762edb4f98e4f919780da108
Erhan Özgüç <eozguc@hotmail.com>
2022-05-31 16:46:32 +0300

    Add files via upload

---
+++ SwapII/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SwapII;
+
+public class Program
+{
+    public static string SwapII(string str) {
+
+       
+
+            // code goes here  
+            for(int i=0 ; i<str.Length ; i++)
+            {
+                if(char.IsLower(str , i))
+                    Console.Write(char.ToUpper(str[i]));
+      
+                else if(char.IsUpper(str , i))
+                    Console.Write(char.ToLower(str[i]));
+      
+                else
+                    Console.Write(str[i]);
+            }
+            return str;
+
+        }
+
+        static void Main() {  
+            // keep this function call here
+            Console.WriteLine(SwapII(Console.ReadLine()));
+        } 
+}
